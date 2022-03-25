2b2d7390765ecfd451d2860cdf31897a0e2f9f28
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2022-03-25 16:03:20 +1300

    how about this

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,6 @@ import * as React from 'react'
 import { motion } from 'framer-motion'
 import styles from './hero.module.css'
 
-import designIcon from '../images/design-icon.svg'
-import mapIcon from '../images/map-icon.svg'
 
 import ClientOnly from "../components/ClientOnly.js"
 
@@ -26,44 +24,7 @@ function Hero(props) {
           </div>
         </div>
 
-        {props.details === true &&
-          <ClientOnly>
-            <div className={styles.innerGrid}>
-              <div className={styles.details}>
-                <div className={styles.detail}>
-                  <motion.div
-                    initial={{ opacity: 0, scale: 0.6, y: 0 }}
-                    animate={{ opacity: 1, scale: 1, y: 0 }}
-                    transition={{ type: 'spring', delay: 0.4, damping: 15 }}
-                    className={styles.iconWrapper}>
-                    <img src={designIcon} alt="Design Layout Icon" className={styles.detailIcon} />
-                  </motion.div>
-                  <motion.h3
-                    className={styles.label}
-                    initial={{ opacity: 0, y: 30 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ type: 'spring', delay: 0.65, damping: 15 }}>
-                    Design Manager at Discord
-                  </motion.h3>
-                </div>
-                <div className={styles.detail}>
-                  <motion.div
-                    initial={{ opacity: 0, scale: 0.6, y: 0 }}
-                    animate={{ opacity: 1, scale: 1, y: 0 }}
-                    transition={{ type: 'spring', delay: 0.8, damping: 15 }}
-                    className={styles.iconWrapper}>
-                    <img src={mapIcon} alt="Map Icon" className={styles.detailIcon} />
-                  </motion.div>
-                  <motion.h3
-                    initial={{ opacity: 0, y: 30 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ type: 'spring', delay: 1, damping: 15 }}
-                    className={styles.label}>San Francisco -> Chicago
-                  </motion.h3>
-                </div>
-              </div>
-            </div>
-          </ClientOnly>
+       
         }
       </div>
     </div>
